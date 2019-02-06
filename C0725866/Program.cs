2c1717a055d9d0d10ca67a7853b9b545d37fd1c3
visualstudio <visualstudio@122-PC31>
2019-02-06 10:12:25 -0500

    CHANGE

--- C0725866/Program.cs
+++ C0725866/Program.cs
@@ -8,18 +8,18 @@ namespace ConsoleApp1
 {
     class Program
     {
-        
+        static int i = 0;
         static void Main(string[] args)
         {
-            
             MethodA();
-
+            Console.ReadLine();
         }
         static void MethodA()
         {
             
             while (Peanut())
             {
+                Console.WriteLine("The value of i is {0}",i);
                 if (i > 10)
                 {
                     return;
